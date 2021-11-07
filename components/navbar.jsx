9b97c62b5b39d6c233cacecda2fa1802270fe0cc
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-07 14:22:43 -0500

    first npm i and navbar component first version

---
+++ components/navbar.jsx
@@ -0,0 +1,13 @@
+import styles from "./Navbar.module.css";
+
+const NavBar = () => {
+    return(
+        <nav className={styles.navbar}>
+        <h2>Hello there</h2>
+      </nav>
+    )
+ 
+};
+
+
+export default NavBar;
