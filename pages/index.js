50c90f9fc850c7515c1c1e3b56e2b8f5902d46e2
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-07 15:39:03 -0500

    finished layout created about page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
-
 export default function Home() {
   return (
     <div className={styles.container}>
       <Layout title="Welcome">
         <h2>Home</h2>
       </Layout>
-      
     </div>
   );
 }
