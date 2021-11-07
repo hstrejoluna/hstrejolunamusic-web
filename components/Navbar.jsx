840728f1cfead4165501cd2f57aa4f3babdc46ee
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-07 16:25:36 -0500

    navbar routing finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,44 @@
 import styles from "./Navbar.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
-    return(
-        <div className={styles.navbar}>
-        <h2>Hello there</h2>
-        <nav>
-            <ul className={styles.navigation}>
-                <li className={styles.sweeptop}>Music</li>
-                <li className={styles.sweeptop}>About</li>
-                <li className={styles.sweeptop}>Blog</li>
-            </ul>
-        </nav>
-      </div>
-    )
- 
-};
+  const router = useRouter();
 
+  return (
+    <div className={styles.navbar}>
+      <h2>Hello there</h2>
+      <nav className={styles.navigation}>
+        <Link href="/">
+          <a
+            className={
+              router.pathname === "/" ? styles.active : styles.sweeptop
+            }
+          >
+            Music
+          </a>
+        </Link>
+        <Link href="/about">
+          <a
+            className={
+              router.pathname === "/about" ? styles.active : styles.sweeptop
+            }
+          >
+            About
+          </a>
+        </Link>
+        <Link href="/blog">
+          <a
+            className={
+              router.pathname === "/blog" ? styles.active : styles.sweeptop
+            }
+          >
+            Blog
+          </a>
+        </Link>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
