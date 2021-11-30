bfb1eb60672e58d52dbeb6c9f1500ad40a0ed6ac
hstrejoluna <trejolunatutoriales@gmail.com>
2021-11-30 15:06:31 -0500

    removed everything about icomoon

--- components/AboutCard.jsx
+++ components/AboutCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./AboutCard.module.css";
-import Icon from "../icon";
 
 const AboutCard = () => {
   return (
@@ -12,7 +11,6 @@ const AboutCard = () => {
         height="200"
         alt="profile pic"
       />
-        <Icon icon="github" size={120} color="orange"  />
       <div className={styles.about}>
         <h2>HS Trejo Luna</h2>
         <p>Cancún, México</p>
@@ -22,8 +20,6 @@ const AboutCard = () => {
           Software Environment.
         </p>
         <p>My Networks</p>
-      
-
       </div>
     </div>
   );
--- icon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import IcoMoon from "react-icomoon";
-const iconSet = require("./webicon.json");
-
-const Icon = ({ ...props }) => {
-  return <IcoMoon iconSet={iconSet} {...props} />;
-};
-
-export default Icon;
